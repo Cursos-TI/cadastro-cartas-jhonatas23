366c53a5e9622b90565ade013708957ce28e45e4
jhonatas23 <bbsq54@gmail.com>
2026-04-17 22:38:13 +0000

    terminei o desafio super trunfo nivel avançado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das cartas
-// Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
-
 int main(){
   // Área para definição das variáveis para armazenar as propriedades das cidades
   char estado[50];
-  char codigo[30], nome[30];
+  char codigo1[30], nome[30], codigo2[30];
   int numero;
-  float area, densidade;
+  int pontos1, pontos2;
+  float area, densidade, super;
   double pib, pibp, populaçao;
 
   // Área para entrada de dados
@@ -18,7 +15,7 @@ int main(){
   getchar(); // limpar o enter que ficou no buffer
 
   printf("Código da carta:\n");
-  scanf("%s", codigo);
+  scanf("%s", codigo1);
   getchar(); // limpar o enter que ficou no buffer
 
   printf("Nome da Cidade:\n");
@@ -46,30 +43,33 @@ int main(){
 
   pibp = pib / populaçao;
 
+  super = populaçao + area + pib + pibp + numero + (1.0 / densidade);
+
   printf("\n\n"); // pula duas linhas
   
   // Área para exibição dos dados da cidade
   printf("Estado: %s\n", estado);
-  printf("Codigo da carta: %s\n", codigo);
+  printf("Codigo da carta: %s\n", codigo1);
   printf("Nome da cidade: %s\n", nome);
   printf("População %.0lf\n", populaçao);
   printf("Área em km²: %.0f\n", area);
   printf("Pib: %.0lf\n", pib);
   printf("Número de pontos turísticos: %d\n", numero);
   printf("Densidade Populacional: %.3f\n", densidade);
-  printf("Pib Per Capita: %.0lf\n\n\n", pibp);
+  printf("Pib Per Capita: %.0lf\n", pibp);
+  printf("Super poder: %.0f\n\n\n", super);
 
   printf("Estado:\n");
   scanf("%s", estado);
   getchar(); // limpar o enter que ficou no buffer
 
   printf("Código da carta:\n");
-  scanf("%s", codigo);
+  scanf("%s", codigo2);
   getchar(); // limpar o enter que ficou no buffer
 
   printf("Nome da cidade:\n");
   scanf("%s", nome);
-  getchar(); // limpar o enter do buffer
+  getchar(); // limpar o enter que ficou no buffer
 
   printf("População:\n");
   scanf("%lf", &populaçao);
@@ -91,11 +91,13 @@ int main(){
   densidade = populaçao / area;
 
   pibp = pib / populaçao;
+  
+  super = (populaçao + area + pib + pibp + numero) + 1/densidade;
 
   printf("\n\n"); // pula duas linhas
 
   printf("Estado: %s\n", estado);
-  printf("Código da carta: %s\n", codigo);
+  printf("Código da carta: %s\n", codigo2);
   printf("Nome da cidade: %s\n", nome);
   printf("População: %.0lf\n", populaçao);
   printf("Área em km²: %.0f\n", area);
@@ -103,6 +105,58 @@ int main(){
   printf("Números de pontos turísticos: %d\n", numero);
   printf("Densidade Populacional: %.3f\n", densidade);
   printf("Pib Per Capita: %.0lf\n", pibp);
+  printf("Super poder: %.0f\n", super);
+
+  printf("\n\n"); // pular duas linhas
+
+  
+  // comparação das cartas
+  printf("Comparação das cartas\n");
+   if (populaçao > populaçao) {
+    printf("Carta %s venceu\n", codigo1);
+   } else  {
+    printf("carta %s venceu\n", codigo2);
+   }
+   if (area > area) {
+    printf("Carta %s venceu\n", codigo1);
+   } else  {
+    printf("Carta %s venceu\n", codigo2);
+   }
+   if (pib > pib) {
+    printf("Carta %s venceu\n", codigo1);
+   } else {
+    printf("Carta %s venceu\n", codigo2);
+   }
+   if (numero > numero) {
+    printf("Carta %s venceu\n", codigo1);
+   } else {
+    printf("Carta %s venceu\n", codigo2);
+   }
+   if (densidade > densidade) {
+    printf("Carta %s venceu\n", codigo2);
+   } else {
+    printf("Carta %s venceu\n", codigo1);
+   }
+   if (pibp > pibp) {
+    printf("Carta %s venceu\n", codigo1);
+   } else {
+    printf("Carta %s venceu\n", codigo2);
+   }
+   if (super > super) {
+    printf("Carta %s venceu\n", codigo1);
+   } else {
+    printf("Carta %s venceu\n", codigo2);
+   }
+   printf("\n\n");
+   
+   if (populaçao > populaçao && area > area && pib > pib && numero > numero && densidade > densidade && pibp > pibp && super > super) {
+    printf("A Grande Campeã foi a Carta %s\n", codigo1);
+   } else {
+    printf("A Grande Campeã foi a Carta %s\n", codigo2);
+   }
+  
+
 
 return 0;
-} 
+
+}
